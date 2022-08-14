81b9ab9b6ce2d6004ca921ce25edb525639842e1
wumeenna <minu0604@naver.com>
2022-08-14 20:09:02 +0900

    chart

--- src/api.ts
+++ src/api.ts
@@ -17,3 +17,13 @@ export const fetchCoinTickers = async (coinId: string) => {
     .get(`${BASE_URL}/tickers/${coinId}`)
     .then((response) => response.data);
 };
+
+export const fetchCoinHistory = async (coinId: string) => {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 66 * 60 * 24;
+  return await axios
+    .get(
+      `${BASE_URL}/coins/${coinId}/ohlcv/historical?start=${startDate}&end=${endDate}`
+    )
+    .then((response) => response.data);
+};
